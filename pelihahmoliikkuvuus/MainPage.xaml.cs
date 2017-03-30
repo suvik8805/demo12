db9baffae091ba2fb5f173bce9fa6d438431290e
K8805 <K8805@OHJE-330-WS-24>
2017-03-30 15:06:40 +0300

    Omaan peliin pohja pelihahmon liikkeelle

--- pelihahmoliikkuvuus/MainPage.xaml.cs
+++ pelihahmoliikkuvuus/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,81 @@ namespace pelihahmoliikkuvuus
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Pelihahmo pelihahmo;
+
+        // näppäinten arvot ylös/alas
+        private bool LeftPressed;
+        private bool RightPressed;
+
+        //pelilloopi
+        private DispatcherTimer timer;
+       
+
+        public object SpriteSheetOffset { get; private set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            // hahmo taustaan
+
+            pelihahmo = new Pelihahmo();
+            
+            Tausta.Children.Add(pelihahmo);
+
+            //näppäimet alas/ylös
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+
+            //pelilooppi
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            timer.Tick += Timer_Tick;
+            timer.Start(); 
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            
+
+
+            if (LeftPressed) pelihahmo.Move(-1);
+            if (RightPressed) pelihahmo.Move(1);
+
+            //päivitä sijainti
+            pelihahmo.SetLocation();
+
+        }
+
+        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    LeftPressed = false;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = false;
+                    break;
+
+
+            }
+
+        }
+
+        private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    LeftPressed = true;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = true;
+                    break;
+
+
+            }
         }
     }
 }
---
+++ pelihahmoliikkuvuus/Pelihahmo.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace pelihahmoliikkuvuus
+{
+    public sealed partial class Pelihahmo : UserControl
+    { 
+    
+        // animaation timer
+        private DispatcherTimer timer;
+
+        // Hahmon näkyminen
+        private int currentFrame = 0;
+        private int direction = 1; // 1 tai -1, -1 vasen, 1 oikee, vrt koordinaatisto
+        private int frameHeight = 40; // objektin koko
+
+        //vauhti
+        private readonly double Maxspeed = 10; //maksimivauhti
+        private readonly double Accelerate = 1; // kiihtyvyys
+        private double speed;
+
+        private readonly double AngleStep = 5;
+      
+
+
+        public Pelihahmo()
+        {
+            this.InitializeComponent();
+
+            //animaatio
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 125);
+            // millisekunnit
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        //sijainti x ja y -akselit
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+        public object ButterflyRotateAngle { get; private set; }
+        public object SpriteSheetOffset { get; private set; }
+
+        //kääntymisen arvot
+        private double Angle = 0; // tulee objektin määrittelystä
+
+        internal void Move(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            SpriteSheetOffset.Y = currentFrame * -frameHeight;
+        }
+
+        //vauhti
+        public void Move()
+        {
+            speed += Accelerate;
+            if (speed > Maxspeed) speed = Maxspeed;
+
+            Angle += direction * AngleStep; //-1*5 or 1*5
+  
+        }
+
+        // sijainnin päivitys
+        public void SetLocation()
+        {
+            SetValue(Canvas.LeftProperty, LocationX); // vaakalinja
+            SetValue(Canvas.TopProperty, LocationY); // pystylinja
+        }
+        
+
+    }
+}
+
+    
+    
+    
+
